49063a2fd6005fbc5aaa41b8275f294cb4fb6418
Hunter Trujillo <cryptoquick@pm.me>
2024-08-19 05:13:29 -0600

    Refactor to use Bitcoin Core JSON-RPC. MUCH FASTER!!

--- src/main.rs
+++ src/main.rs
@@ -2,140 +2,53 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 
-use bitcoin::{
-    block::Version, consensus::deserialize, Block, BlockHash, CompactTarget, TxMerkleNode,
+use anyhow::Result;
+use bitcoincore_rpc::{
+    json::{GetChainTipsResultStatus, GetChainTipsResultTip},
+    Auth, Client, RpcApi,
 };
-use chrono::{TimeZone, Utc};
+use chrono::{Duration, TimeZone, Utc};
 use indicatif::ProgressBar;
-use reqwest::blocking::Client;
-use serde::{Deserialize, Serialize};
 
-#[derive(Copy, PartialEq, Eq, Clone, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-pub struct Header {
-    /// Block version, now repurposed for soft fork signalling.
-    pub version: Version,
-    /// Reference to the previous block in the chain.
-    #[serde(alias = "previousblockhash")]
-    pub prev_blockhash: BlockHash,
-    /// The root hash of the merkle tree of transactions in the block.
-    pub merkle_root: TxMerkleNode,
-    /// The timestamp of the block, as claimed by the miner.
-    #[serde(alias = "timestamp")]
-    pub time: u32,
-    /// The target value below which the blockhash must lie.
-    pub bits: CompactTarget,
-    /// The nonce, selected to obtain a low enough blockhash.
-    pub nonce: u32,
-}
-
-impl Header {
-    /// Convert the `time` field to a date string in the format `MM/DD/YYYY`.
-    pub fn formatted_date(&self) -> String {
-        let datetime = Utc
-            .timestamp_opt(self.time as i64, 0)
-            .single()
-            .expect("Invalid timestamp");
-
-        datetime.format("%m/%d/%Y %H:%M:%S").to_string()
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Transaction {
-    pub vsize: u32,
-    #[serde(rename = "feePerVsize")]
-    pub fee_per_vsize: f64,
-    #[serde(rename = "effectiveFeePerVsize")]
-    pub effective_fee_per_vsize: f64,
-    pub txid: String,
-    pub version: u32,
-    pub locktime: u32,
-    pub size: u32,
-    pub weight: u32,
-    pub fee: u64,
-    pub vin: Vec<Vin>,
-    pub vout: Vec<Vout>,
-    pub status: Status,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Vin {
-    pub is_coinbase: bool,
-    pub prevout: Option<Prevout>,
-    pub scriptsig: String,
-    pub scriptsig_asm: String,
-    pub sequence: u32,
-    pub txid: String,
-    pub vout: u32,
-    pub witness: Vec<String>,
-    pub inner_redeemscript_asm: String,
-    pub inner_witnessscript_asm: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Prevout {
-    pub value: u64,
-    pub scriptpubkey: String,
-    pub scriptpubkey_address: String,
-    pub scriptpubkey_asm: String,
-    pub scriptpubkey_type: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Vout {
-    pub value: u64,
-    pub scriptpubkey: String,
-    pub scriptpubkey_address: String,
-    pub scriptpubkey_asm: String,
-    pub scriptpubkey_type: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Status {
-    pub confirmed: bool,
-    pub block_height: u64,
-    pub block_hash: String,
-    pub block_time: u64,
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     let mut out: Vec<String> = vec![];
     out.push("Date,Total P2PK addresses,Total P2PK coins".to_owned());
 
-    let onion = env::var("ONION")?;
+    let url = env::var("URL")?;
+    let cookie = env::var("COOKIE");
+    let auth = match cookie {
+        Ok(cookiefile) => Auth::CookieFile(cookiefile.into()),
+        Err(_) => {
+            let user = env::var("USER")?;
+            let pass = env::var("PASS")?;
 
-    let client = Client::builder()
-        .danger_accept_invalid_certs(true)
-        .build()?;
+            Auth::UserPass(user, pass)
+        }
+    };
+    let rpc = Client::new(&url, auth)?;
 
     let mut p2pk_addresses: i32 = 0;
     let mut p2pk_coins: f64 = 0.0;
 
-    let url = format!("https://{onion}.local/api/blocks/tip/height");
-    let response = client.get(url).send()?.text()?;
-    let height = response.parse()?;
+    let result = rpc.get_chain_tips()?;
+    let tip_height = result
+        .iter()
+        .filter(|fork: &&GetChainTipsResultTip| fork.status == GetChainTipsResultStatus::Active)
+        .collect::<Vec<_>>()
+        .first()
+        .unwrap()
+        .height;
 
-    let pb = ProgressBar::new(height);
+    let pb = ProgressBar::new(tip_height);
+    pb.println(format!("Syncing from blocks 1 to {tip_height}"));
 
-    pb.println(format!("Syncing from blocks 1 to {height}"));
-
-    for i in 1..height {
-        let url = format!("https://{onion}.local/api/block-height/{i}");
-        pb.println(&url);
-        let hash = client.get(url).send()?.text()?;
-        let url = format!("https://{onion}.local/api/block/{hash}");
-        pb.println(&url);
-        let response = client.get(url).send()?.text()?;
-        let json: Header = serde_json::from_str(&response)?;
-        // println!("{}", serde_json::to_string_pretty(&json)?);
-        let url = format!("https://{onion}.local/api/block/{hash}/raw");
-        let response = client.get(url).send()?.bytes()?;
-        let block: Block = deserialize(&response)?;
+    for height in 1..tip_height {
+        let hash = rpc.get_block_hash(height)?;
+        let block = rpc.get_block(&hash)?;
 
         for (i, tx) in block.txdata.iter().enumerate() {
             for outpoint in &tx.output {
-                let out_script = outpoint.script_pubkey.to_asm_string();
-                if out_script.starts_with("OP_PUSHBYTES_65") {
+                if outpoint.script_pubkey.is_p2pk() {
                     p2pk_addresses += 1;
                     p2pk_coins += outpoint.value.to_btc();
                 }
@@ -144,37 +57,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             if i > 1 {
                 for txin in &tx.input {
                     let txid = txin.previous_output.txid;
-                    let url = format!("https://{onion}.local/api/tx/{txid}");
-                    pb.println(&url);
-                    let response = client.get(url).send()?.text()?;
-                    pb.println(&response);
-                    let transaction: Transaction = serde_json::from_str(&response)?;
+                    let transaction = rpc.get_raw_transaction(&txid, None)?;
 
-                    for vin in transaction.vin {
-                        if vin.is_coinbase {
-                            continue;
-                        }
+                    if transaction.is_coinbase() {
+                        continue;
                     }
 
-                    for outpoint in transaction.vout {
-                        let out_script = outpoint.scriptpubkey_asm;
-                        if out_script.starts_with("OP_PUSHBYTES_65") {
+                    for outpoint in transaction.output {
+                        if outpoint.script_pubkey.is_p2pk() {
                             p2pk_addresses -= 1;
-                            p2pk_coins -= (outpoint.value as f64) / 100_000_000.0;
+                            p2pk_coins -= outpoint.value.to_btc();
                         }
                     }
                 }
             }
         }
 
+        let datetime = Utc
+            .timestamp_opt(block.header.time as i64, 0)
+            .single()
+            .expect("Invalid timestamp");
+
+        let formatted_date = datetime.format("%m/%d/%Y %H:%M:%S").to_string();
+
         out.push(format!(
             "{},{},{}",
-            json.formatted_date(),
-            p2pk_addresses,
-            p2pk_coins
+            formatted_date, p2pk_addresses, p2pk_coins
         ));
 
         pb.inc(1);
+
+        let eta_duration = pb.eta();
+        let eta_datetime =
+            Utc::now() + Duration::from_std(eta_duration).unwrap_or_else(|_| Duration::seconds(0));
+        let eta_hms = eta_datetime.format("%H:%M:%S").to_string();
+
+        pb.println(format!("Block: {height} - ETA: {eta_hms}"));
     }
 
     let content = out.join("\n");
